4b5dd5159556a121db84ff4e5a3c0f6b40596ec6
bhardwajaavi <bhardwajaavi@gmail.com>
2025-11-16 09:03:25 +0530

    initial commit

--- tickets/urls.py
+++ tickets/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', views.custom_logout_view, name='logout'),
-    path('register/', views.register_view, name='register'),
+    path('register/', views.register_view(template_name='register.html'), name='register'),
 ]
